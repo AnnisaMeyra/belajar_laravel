247ae2d4f44b5ab294bc224f5eb38559c7fa6b5e
Annisa Meyra <anisamaira08721@gmail.com>
2025-07-03 18:54:27 +0700

    Update komponen navbar dan tambahkan foto profil

---
+++ api/index.php
@@ -0,0 +1,16 @@
+<?php
+
+// Autoload Composer dependencies
+require __DIR__ . '/../vendor/autoload.php';
+
+// Bootstrap Laravel
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$request = Illuminate\Http\Request::capture();
+$response = $kernel->handle($request);
+
+$response->send();
+
+$kernel->terminate($request, $response);
